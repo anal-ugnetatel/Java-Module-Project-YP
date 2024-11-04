61b8e1578f509c232619bfc212f07f5483f2b138
anal-ugnetatel <test@test.ru>
2024-11-04 15:16:59 +0300

    Исправление фильтра ввода скорости.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,24 @@ public class Main {
         for (int i=0;i<3;i++){
             System.out.println("— Введите название машины №"+(i+1)+": ");
             String name = scanner.next();
-            System.out.println("— Введите скорость машины №"+(i+1)+": ");
-            int speed = scanner.nextInt();
-            while (speed<1||speed>250){
-                System.out.println("— Неправильная скорость");
+            int speed;
+
+
+
+            while (true){
+
                 System.out.println("— Введите скорость машины №"+(i+1)+": ");
-                speed = scanner.nextInt();
+
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    if(speed>0&&speed<251){
+                        break;
+                    }
+                }
+                System.out.println("— Неправильная скорость");
+                scanner.nextLine();
             }
+
             carList.add(new Car(name, speed));
         }
         Race raceResult = new Race();
