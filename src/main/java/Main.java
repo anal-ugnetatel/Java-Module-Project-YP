882e5a5fa2392b09560318b30ff6360a47d12da2
anal-ugnetatel <test@test.ru>
2024-11-04 14:15:52 +0300

    Выполненное первое домашнее задание.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> carList = new ArrayList<>(3);
+        for (int i=0;i<3;i++){
+            System.out.println("— Введите название машины №"+(i+1)+": ");
+            String name = scanner.next();
+            System.out.println("— Введите скорость машины №"+(i+1)+": ");
+            int speed = scanner.nextInt();
+            while (speed<1||speed>250){
+                System.out.println("— Неправильная скорость");
+                System.out.println("— Введите скорость машины №"+(i+1)+": ");
+                speed = scanner.nextInt();
+            }
+            carList.add(new Car(name, speed));
+        }
+        Race raceResult = new Race();
+        raceResult.resultRace(carList);
     }
+
 }
